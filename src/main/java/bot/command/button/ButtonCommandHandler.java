0883798dff37cdaee84e4ff36d137a91c1232bef
4v0n <navodhk@hotmail.com>
2023-07-12 23:52:15 +0100

    Clean up

--- src/main/java/bot/command/button/ButtonCommandHandler.java
+++ src/main/java/bot/command/button/ButtonCommandHandler.java
@@ -1,15 +1,20 @@
-package bot.command.button;
+package bot.command.Button;
 
-import bot.listener.ButtonListener;
+import bot.listener.ButtonInteractionListener;
+import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.interactions.components.ItemComponent;
+import net.dv8tion.jda.api.interactions.components.buttons.Button;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.concurrent.TimeUnit;
 
 public abstract class ButtonCommandHandler {
     private final ButtonCommandCenter commandCenter;
 
-    public ButtonCommandHandler(Message controller) {
-        commandCenter = new ButtonCommandCenter(controller);
+    public ButtonCommandHandler() {
+        commandCenter = new ButtonCommandCenter();
     }
 
     public abstract void buildCommands();
@@ -22,14 +27,13 @@ public abstract class ButtonCommandHandler {
         return commandCenter.getController();
     }
 
-    public void activate(int activeTimeinMins) {
-        ButtonListener buttonListener = ButtonListener.getINSTANCE();
-        buttonListener.addCommandCenter(commandCenter);
-        buildCommands();
+    public void activate(int activeTimeInMins) {
+        ButtonInteractionListener listener = ButtonInteractionListener.getINSTANCE();
+        listener.addCommandCenter(commandCenter);
 
         Thread waitThread = new Thread(() -> {
             try {
-                Thread.sleep(TimeUnit.MINUTES.toMillis(activeTimeinMins));
+                Thread.sleep(TimeUnit.MINUTES.toMillis(activeTimeInMins));
                 deactivate();
             } catch (InterruptedException e) {
                 e.printStackTrace();
@@ -40,8 +44,28 @@ public abstract class ButtonCommandHandler {
     }
 
     public void deactivate() {
-        ButtonListener buttonListener = ButtonListener.getINSTANCE();
-        buttonListener.removeCommandCenter(commandCenter);
-        getController().getButtons().clear();
+        ButtonInteractionListener listener = ButtonInteractionListener.getINSTANCE();
+        listener.removeCommandCenter(commandCenter);
+
+        Guild guild = getController().getGuild();
+        guild.getTextChannelById(getController().getChannel().getId()).retrieveMessageById(getController().getId()).queue(msg -> {
+            ArrayList<Button> disabledButtons = new ArrayList<>();
+            for (ItemComponent oldButton : getButtons()) {
+                disabledButtons.add(((Button) oldButton).asDisabled());
+            }
+            try {
+                msg.editMessage(msg.getContentRaw()).setActionRow(disabledButtons).queue();
+            } catch (IllegalArgumentException e) {
+                msg.editMessageEmbeds(msg.getEmbeds()).setActionRow(disabledButtons).queue();
+            }
+        });
+    }
+
+    public Collection<? extends ItemComponent> getButtons() {
+        return commandCenter.getButtons();
+    }
+
+    public void setController(Message message) {
+        commandCenter.setController(message);
     }
 }
